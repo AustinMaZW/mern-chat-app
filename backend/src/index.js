3a10c794bf3d76fd819744c9b610207cfac7cf82
Austin Ma <austin.ma@oracle.com>
2025-09-18 16:46:44 -0700

    regex error update for serving static file

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,8 +29,9 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  // fallback route (regex catch-all)
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,7 +19,7 @@ backdrop-blur-lg bg-base-100/80"
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
-              <h1 className="text-lg font-bold">Chatty</h1>
+              <h1 className="text-lg font-bold">RaRaChat</h1>
             </Link>
           </div>
 
